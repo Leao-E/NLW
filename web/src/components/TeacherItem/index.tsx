5c58b2fd2ab4e469e31ca9de56b229473f21ab87
emanuelleao <eemanuelleao@gmail.com>
2020-08-06 22:22:09 -0300

    TeacherItem is now a individual component

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 function TeacherItem () {
-  return (<h1></h1>);
+  return (
+    <article className="teacher-item">  
+      <header>
+        <img src="https://ipc.digital/wp-content/uploads/2016/07/icon-user-default.png" alt="Nome do Usuário"/>
+        
+        <div>
+          <strong>Nome do Usuário</strong>
+          <span>Nome da Matéria</span>
+        </div>
+      </header>
+
+      <p>
+        sdxcfgvhbjnlasde e ebh bl vlhj vj vjgv jv lv lv lgjv lgjv 
+        <br/>
+        lk fktf td hdj ydj d rdr j td tdk tk 
+      </p>
+      <footer>
+        <p>
+          Preço/Hora
+          <strong>R$ 60,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="WhatsApp"/>
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
 }
 
 export default TeacherItem;
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
 
@@ -29,33 +29,9 @@ function TeacherList () {
       </PageHeader>
 
       <main>
-        <article className="teacher-item">  
-          <header>
-            <img src="https://ipc.digital/wp-content/uploads/2016/07/icon-user-default.png" alt="Nome do Usuário"/>
-            
-            <div>
-        
-              <strong>Nome do Usuário</strong>
-              <span>Nome da Matéria</span>
-            </div>
-          </header>
-
-          <p>
-            sdxcfgvhbjnlasde e ebh bl vlhj vj vjgv jv lv lv lgjv lgjv 
-            <br/>
-            lk fktf td hdj ydj d rdr j td tdk tk 
-          </p>
-          <footer>
-            <p>
-              Preço/Hora
-              <strong>R$ 60,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="WhatsApp"/>
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem/>
+        <TeacherItem/>
+        <TeacherItem/>
       </main>
     </div>
   );
