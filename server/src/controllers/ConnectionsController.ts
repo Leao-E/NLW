f67e47ce9f662e75a07b8e595127ee5c65367cb2
emanuelleao <eemanuelleao@gmail.com>
2020-08-09 20:46:04 -0300

    server: fix table names in Classes and Connections controllers

--- server/src/controllers/ConnectionsController.ts
+++ server/src/controllers/ConnectionsController.ts
@@ -3,7 +3,7 @@ import db from '../database/connection';
 
 export default class ConnectionsController {
   async index (req: Request, res: Response){
-    const totalConnections = await db('connections').count('* as total');
+    const totalConnections = await db('connection').count('* as total');
 
     const { total } = totalConnections[0];
 
@@ -11,9 +11,9 @@ export default class ConnectionsController {
   }
 
   async create (req: Request, res: Response){
-    const { user_id } = req.body;
+    const { user_id } = req.body;      
 
-    await db('connections').insert({
+    await db('connection').insert({
       user_id,
     });
 
