b10b6f13f5b7360d0466b2167cfdfae90b0c64b8
emanuelleao <eemanuelleao@gmail.com>
2020-08-14 00:06:20 -0300

    mobile: changed Landing page buttons

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -5,11 +5,22 @@ import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
 import Br from '../../Components/Utils/Br';
 
+
 const Landing = () => {
+
+  const {navigate} = useNavigation();
+
+
+  function handleNavigateToGiveClassesPage () {
+    navigate('GiveClasses');
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner}/>  
@@ -21,14 +32,14 @@ const Landing = () => {
         </Text>
       </Text>
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+        <RectButton style={[styles.button, styles.buttonPrimary]}>
           <Image source={studyIcon}/>
           <Text style={styles.buttonText}>Estudar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+        </RectButton>
+        <RectButton onPress={handleNavigateToGiveClassesPage} style={[styles.button, styles.buttonSecondary]}>
           <Image source={giveClassesIcon}/>
-          <Text style={styles.buttonText}>Estudar</Text>
-        </TouchableOpacity>
+          <Text style={styles.buttonText}>Dar aulas</Text>
+        </RectButton>
       </View>
       <Text style={styles.totalConnection}>
         Total de xxx conexões já realizadas {'  '} <Image source={heartIcon}/>
