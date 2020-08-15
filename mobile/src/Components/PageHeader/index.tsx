c4241c39d34e3537a053a8a97c772fe4272b11df
emanuelleao <eemanuelleao@gmail.com>
2020-08-15 07:45:09 -0300

    mobile: created pageHeader component

---
+++ mobile/src/Components/PageHeader/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler'
+import styles from './styles';
+
+import backIcon from '../../assets/images/icons/back.png';
+import logoImg from '../../assets/images/logo.png';
+import { useNavigation } from '@react-navigation/native';
+
+function PageHeader () {
+
+  const { navigate } = useNavigation();
+
+  function handleGoBack (){
+    navigate('Landing');
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <BorderlessButton onPress={handleGoBack}>
+          <Image source={backIcon} resizeMode='contain'/>       
+        </BorderlessButton>    
+        <Image source={logoImg} resizeMode='contain'/>             
+      </View>
+    </View>
+  );
+}
+
+export default PageHeader;
---
+++ mobile/src/Components/PageHeader/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 60,
+    paddingHorizontal: 40,
+    paddingBottom: 40,
+    backgroundColor: "#8257e5"
+  },
+  topBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  }
+});
+
+export default styles;
