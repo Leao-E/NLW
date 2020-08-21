6af01f1546dedfa8935f8a26d5168f69e4685e73
emanuelleao <eemanuelleao@gmail.com>
2020-08-21 00:45:48 -0300

    mobile: finished TeacherItem

--- mobile/src/Components/TeacherItem/index.tsx
+++ mobile/src/Components/TeacherItem/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+
 import { View, Image, Text } from 'react-native';
+import Br from '../Utils/Br';
+import { RectButton } from 'react-native-gesture-handler';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
 import styles from './styles';
 
 function TeacherItem () {
@@ -13,6 +21,28 @@ function TeacherItem () {
         <View style={styles.profileInfo}>
           <Text style={styles.name}>Emanuel Leão</Text>
           <Text style={styles.subject}>React Native</Text>
+        </View>
+      </View>
+      <Text style={styles.bio}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu blandit nisi.   
+        <Br/><Br/>
+        In condimentum, dolor id molestie scelerisque, nunc justo efficitur mi, vehicula aliquam justo magna nec ante. Sed eros quam, interdum sit amet nisi nec, mollis tristique dolor.
+      </Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preço/hora{'   '}
+          <Text style={styles.priceValue}>R$ 20,00</Text>
+        </Text>
+
+        <View style={styles.buttonsContainer}>
+          <RectButton style={styles.favoriteButton}>
+            <Image source={heartOutlineIcon} />
+          </RectButton>
+          <RectButton style={styles.contactButton}>
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>Entrar em contato</Text>
+          </RectButton>
         </View>
       </View>
     </View>
