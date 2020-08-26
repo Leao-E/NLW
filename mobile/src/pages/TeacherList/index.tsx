3c88a93b1e179343219c2459558837d5c96f9b2c
emanuelleao <eemanuelleao@gmail.com>
2020-08-25 23:39:01 -0300

    mobile: added search form

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, Text } from 'react-native';
 
 import PageHeader from '../../Components/PageHeader';
 import TeacherItem from '../../Components/TeacherItem';
 
 import styles from './styles';
+import { TextInput } from 'react-native-gesture-handler';
 
 function TeacherList (){
   return (
     <View style={styles.container} >
-      <PageHeader title={'Proffys disponíveis'}/>      
+      <PageHeader title={'Proffys disponíveis'}>      
+        <View style={styles.searchForm}>
+          <Text style={styles.label}>Matéria</Text>
+          <TextInput 
+            placeholderTextColor="#c1bccc"
+            style={styles.input}
+            placeholder="Seleciona a matéria"
+          />   
+          <View style={styles.inputGroup}>
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Dia da semana</Text>
+              <TextInput
+                placeholderTextColor="#c1bccc"
+                style={styles.input}
+                placeholder="Selecione o dia"
+              />
+            </View>
+            
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Horário</Text>
+              <TextInput
+                placeholderTextColor="#c1bccc" 
+                style={styles.input}
+                placeholder="Selecione horário"
+              />
+            </View>
+          </View>       
+        </View>  
+      </PageHeader>
         <ScrollView
           style={styles.teacherList}
 
