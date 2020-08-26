a5a1b2040abc35047a311006f4fc567ac9eff2e0
emanuelleao <eemanuelleao@gmail.com>
2020-08-26 01:22:45 -0300

    mobile: added ReactNode to PageHeader

--- mobile/src/Components/PageHeader/index.tsx
+++ mobile/src/Components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler'
 import styles from './styles';
@@ -8,10 +8,11 @@ import logoImg from '../../assets/images/logo.png';
 import { useNavigation } from '@react-navigation/native';
 
 interface PageHeaderProps {
-  title?: string,
+  title: string,
+  headerRight?: ReactNode,
 };
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title, children, ...rest }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, headerRight, children, ...rest }) => {
 
   const { navigate } = useNavigation();
 
@@ -27,9 +28,13 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title, children, ...rest }) => 
         </BorderlessButton>    
         <Image source={logoImg} resizeMode='contain'/>             
       </View>
-      <Text style={styles.title}>
-        {title}
-      </Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>
+          {title}
+        </Text>
+        {headerRight}
+      </View>
+    
       {children}
     </View>
   );
