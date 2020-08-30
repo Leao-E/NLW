3993c612e60eb727cc8c09b7070953a394de4265
emanuelleao <eemanuelleao@gmail.com>
2020-08-30 16:25:08 -0300

    mobile: TeacherItem component now has favorited prop and the handleFavoriteButton now works with AsyncStorage

--- mobile/src/Components/TeacherItem/index.tsx
+++ mobile/src/Components/TeacherItem/index.tsx
@@ -8,6 +8,7 @@ import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
 import styles from './styles';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export interface Teacher{
   id: number,
@@ -21,12 +22,30 @@ export interface Teacher{
 
 interface TeacherItemProps {
   teacher: Teacher,
+  favorited: boolean
 }
 
 
-const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
 
-  function handleFavoriteButton(){
+  async function handleFavoriteButton(){    
+    const favorites = await AsyncStorage.getItem('favorites');
+      
+    let favoritesArray: Teacher[] = [];
+    
+    if (favorites){
+      favoritesArray = JSON.parse(favorites);
+    }
+
+    if (isFavorited){
+      const favoriteIndex = favoritesArray.findIndex((teacherItem: Teacher) => {
+        return teacherItem.id === teacher.id;
+      });
+      favoritesArray.splice(favoriteIndex, 1);
+    }else{  
+      favoritesArray.push(teacher);    
+    }
+    await AsyncStorage.setItem('favorites', JSON.stringify(favoritesArray));
     setIsFavorited(!isFavorited);
   }
 
@@ -34,7 +53,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     Linking.openURL(`whatsapp://send?text=Hello World!&phone=${teacher.whatsapp}`);    
   }
 
-  const [ isFavorited, setIsFavorited ] = useState(false);  
+  const [ isFavorited, setIsFavorited ] = useState(favorited);  
 
   return (
     <View style={styles.container}>
